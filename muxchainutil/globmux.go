b77889df9f24ac3c004f1658437a3e28e5f6e0c7
Stephen Searles <stephen.searles@shipwire.com>
2014-05-07 22:56:24 -0700

    Added documentation and renamed files

---
+++ muxchainutil/globmux.go
@@ -0,0 +1,70 @@
+package muxchainutil
+
+import (
+	"net/http"
+	"strings"
+)
+
+// GlobMux muxes patterns with wildcard (*) components.
+type GlobMux struct {
+	paths map[string]http.Handler
+}
+
+// NewGlobMux initializes a new GlobMux.
+func NewGlobMux() *GlobMux {
+	return &GlobMux{paths: make(map[string]http.Handler)}
+}
+
+// Handler accepts a request and returns the appropriate handler for it, along
+// with the pattern it matched. If no appropriate handler is found, the
+// http.NotFoundHandler is returned along with an empty string. GlobMux will
+// choose the handler that matches the most leading path components for the
+// request.
+func (g *GlobMux) Handler(req *http.Request) (h http.Handler, pattern string) {
+	h, pattern = g.match(req.URL.Path)
+	if pattern == "" {
+		return http.NotFoundHandler(), pattern
+	}
+	return
+}
+
+// Handle registers a pattern to a handler.
+func (g *GlobMux) Handle(pattern string, h http.Handler) {
+	g.paths[pattern] = h
+}
+
+// ServeHTTP handles requests with the appropriate globbed handler.
+func (g *GlobMux) ServeHTTP(w http.ResponseWriter, req *http.Request) {
+	h, _ := g.Handler(req)
+	h.ServeHTTP(w, req)
+}
+
+func (g *GlobMux) match(path string) (h http.Handler, pattern string) {
+	longestMatchLen := 0
+	for matchPattern, handler := range g.paths {
+		if pathMatch(matchPattern, path) {
+			matchLen := strings.Count(matchPattern, "/")
+			if matchLen > longestMatchLen {
+				h = handler
+				pattern = matchPattern
+			}
+		}
+	}
+	return
+}
+
+func pathMatch(pattern, path string) bool {
+	pathParts := strings.Split(path, "/")
+	patternParts := strings.Split(path, "/")
+	for i, patternPart := range patternParts {
+		if len(pathParts) > i {
+			switch pathParts[i] {
+			case "*":
+			case patternPart:
+			default:
+				return false
+			}
+		}
+	}
+	return true
+}
---
+++ muxchainutil/pathmux.go
@@ -0,0 +1,52 @@
+package muxchainutil
+
+import (
+	"net/http"
+	"strings"
+)
+
+// PathMux muxes patterns by globbing over variable components and adding those
+// as query parameters to the request for handlers.
+type PathMux struct {
+	*GlobMux
+	patternVariables map[string]map[int]string // maps pattern component index to variable name
+}
+
+// NewPathMuxer initializes a PathMux.
+func NewPathMux() *PathMux {
+	return &PathMux{NewGlobMuxer(), make(map[string]map[int]string)}
+}
+
+// Handle registers a handler to a pattern. Patterns may conatain variable components
+// specified by a leading colon. For instance, "/order/:id" would map to handlers the
+// same as "/order/*" on a GlobMux, however, handlers will see requests as if
+// the query were "/order/?id=".
+func (p *PathMux) Handle(pattern string, h http.Handler) {
+	patternParts := strings.Split(pattern, "/")
+	variables := make(map[int]string)
+	for i, part := range patternParts {
+		if strings.HasPrefix(part, ":") {
+			variables[i] = part[1:]
+			patternParts[i] = "*"
+		}
+	}
+	pattern = strings.Join(patternParts, "/")
+	p.patternVariables[pattern] = variables
+	p.GlobMux.Handle(pattern, h)
+}
+
+// ServeHTTP handles requests by adding path variables to the request and forwarding
+// them to the matched handler. (See Handle)
+func (p *PathMux) ServeHTTP(w http.ResponseWriter, req *http.Request) {
+	req.ParseForm()
+	h, pattern := p.Handler(req)
+	if variables, hasVariables := p.patternVariables[pattern]; hasVariables {
+		for i, part := range strings.Split(req.URL.Path, "/") {
+			if varname, ok := variables[i]; ok {
+				req.URL.Query().Set(varname, part)
+				req.Form.Set(varname, part)
+			}
+		}
+	}
+	h.ServeHTTP(w, req)
+}
