c6f3b12ac886ecc742487c03b7583cf5ae4226ed
Stephen Searles <stephen.searles@gmail.com>
2014-09-04 13:22:04 -0700

    Implementing logging response code and content length

--- muxchainutil/log.go
+++ muxchainutil/log.go
@@ -7,6 +7,8 @@ import (
 	"log"
 	"net/http"
 	"os"
+
+	"github.com/stephens2424/muxchain"
 )
 
 var DefaultLog *LogHandler
@@ -19,8 +21,10 @@ const (
 	Lpath = log.Lshortfile << iota
 	Lmethod
 	LremoteAddr
+	LresponseStatus
+	LcontentLength
 
-	LstdFlags = log.Ldate | log.Ltime | Lmethod | Lpath
+	LstdFlags = log.Ldate | log.Ltime | Lmethod | Lpath | LresponseStatus | LcontentLength
 )
 
 type LogHandler struct {
@@ -33,10 +37,19 @@ func NewLogHandler(out io.Writer, prefix string, flag int) *LogHandler {
 }
 
 func (l LogHandler) ServeHTTP(w http.ResponseWriter, req *http.Request) {
-	l.Println(l.header(req))
+	wr := &responseWriter{ResponseWriter: w}
+	l.Println(l.header(wr, req))
 }
 
-func (l LogHandler) header(req *http.Request) string {
+func (l LogHandler) ServeHTTPChain(w http.ResponseWriter, req *http.Request, h ...http.Handler) {
+	wr := &responseWriter{ResponseWriter: w}
+	defer func() {
+		l.Println(l.header(wr, req))
+	}()
+	muxchain.HandleChain(wr, req, h...)
+}
+
+func (l LogHandler) header(w *responseWriter, req *http.Request) string {
 	buf := &bytes.Buffer{}
 	if Lmethod&l.flag != 0 {
 		fmt.Fprintf(buf, "%s ", req.Method)
@@ -44,5 +57,30 @@ func (l LogHandler) header(req *http.Request) string {
 	if Lpath&l.flag != 0 {
 		fmt.Fprintf(buf, "%s ", req.URL.Path)
 	}
+	if LresponseStatus&l.flag != 0 {
+		fmt.Fprintf(buf, "%d ", w.code)
+	}
+	if LcontentLength&l.flag != 0 {
+		fmt.Fprintf(buf, "%d ", w.written)
+	}
 	return buf.String()
+}
+
+type responseWriter struct {
+	written int
+	code    int
+	http.ResponseWriter
+}
+
+func (r *responseWriter) WriteHeader(code int) {
+	if r.code == 0 {
+		r.code = code
+		r.ResponseWriter.WriteHeader(code)
+	}
+}
+
+func (r *responseWriter) Write(b []byte) (int, error) {
+	n, err := r.ResponseWriter.Write(b)
+	r.written += n
+	return n, err
 }
